d9069d3f62d1697d516c265322a9cd7ce106d49d
David Moraes <investtraderrs@gmail.com>
2020-09-17 12:22:36 -0300

    Exercicio de vetores com referencia

---
+++ ExercicioDeFixacao/Aluguel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao
+{
+    class Aluguel
+    {
+        public String Nome { get; private set; }
+        public String Email { get; private set; }
+
+        public Aluguel(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", "
+                + Email;
+        }
+    }
+}
---
+++ ExercicioDeFixacao/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Quantos Quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Aluguel[] Quartos = new Aluguel[10];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Aluguel #" + (i+1) + ": ");
+                Console.WriteLine();
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Quartos[quarto] = new Aluguel(nome, email);
+                
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 9; i++)
+            {
+                if (Quartos[i] != null)
+                {
+                    Console.Write(i + ": " + Quartos[i]);
+                }
+            }
+        }
+    }
+}
